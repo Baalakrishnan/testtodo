b42a16f63a251a9323c00bf5c8af36abb93556d6
James Rufus Nelavan <nelavan@Jamess-MacBook-Pro.local>
2017-10-29 04:37:32 +0530

    spring and angular project

---
+++ testtodo/src/main/java/com/igttest/testtodo/TesttodoApplication.java
@@ -0,0 +1,12 @@
+package com.igttest.testtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesttodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesttodoApplication.class, args);
+	}
+}
---
+++ testtodo/src/test/java/com/igttest/testtodo/TesttodoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.igttest.testtodo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TesttodoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
